0742b566763c4e0596ba2b0f699a480b10da9891
HironoriSato <maza.hs5360@gmail.com>
2019-05-15 07:27:02 +0900

    [update]Jpaに対応するようEntityクラスを修正

--- src/main/java/com/example/demo/domain/model/Article.java
+++ src/main/java/com/example/demo/domain/model/Article.java
@@ -1,13 +1,29 @@
 package com.example.demo.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
+@Entity
+@Table(name="articles")
 @Data
 public class Article{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
     private Long id;
+    @Column(name="user_id")
     private String userId;
+    @Column(name="user_name")
     private String userName;
+    @Column(name="title")
     private String title;
+    @Column(name="content")
     private String content;
 }
--- src/main/java/com/example/demo/domain/model/User.java
+++ src/main/java/com/example/demo/domain/model/User.java
@@ -1,13 +1,29 @@
 package com.example.demo.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
+@Entity
+@Table(name="users")
 @Data
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="user_id")
     private String userId;
+    @Column(name="password")
     private String password;
+    @Column(name="user_name")
     private String userName;
+    @Column(name="gender")
     private boolean gender;
+    @Column(name="role")
     private String role;
 }
