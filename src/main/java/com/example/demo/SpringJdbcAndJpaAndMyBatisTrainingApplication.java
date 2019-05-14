061948278110b605597e0c0d823ebb09d44ad153
HironoriSato <maza.hs5360@gmail.com>
2019-05-14 22:43:08 +0900

    initial commit

---
+++ src/main/java/com/example/demo/SpringJdbcAndJpaAndMyBatisTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcAndJpaAndMyBatisTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcAndJpaAndMyBatisTrainingApplication.class, args);
+	}
+
+}
