546f6d75ecaa13a6d491d26daa9112572b016fbd
HironoriSato <maza.hs5360@gmail.com>
2019-05-15 07:24:37 +0900

    [update]@Queryを用いたRepositoryクラスによるDB処理を追加

--- src/main/java/com/example/demo/domain/repository/ArticleRepository.java
+++ src/main/java/com/example/demo/domain/repository/ArticleRepository.java
@@ -1,6 +1,10 @@
 package com.example.demo.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.domain.model.Article;
@@ -8,4 +12,9 @@ import com.example.demo.domain.model.Article;
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
+    @Query("SELECT a FROM Article a ORDER BY a.id DESC")
+    List<Article> findAllOrderByIdDesc();
+
+    @Query("SELECT a FROM Article a WHERE a.title = :title")
+    Article findByTitle(@Param("title") String title);
 }
